4de98f2052f04bbfffd4dc3d36642e351bf37577
Spave <excsniper@gmail.com>
2013-02-24 02:54:54 -0500

    Added comments to LinkedListPureStack.java

--- Stack/src/LinkedListPureStack.java
+++ Stack/src/LinkedListPureStack.java
@@ -41,37 +41,41 @@ public class LinkedListPureStack implements PureStack
 	@Override
 	public void push(Object element)
 	{
-		topNode = new Node(element, topNode);
-		size++;
-	}
+		topNode = new Node(element, topNode);	//pushing by setting the new node as the top node and adding a link to the
+												//previous top node
+		size++;									//increasing the size counter
+	}	
 
 	@Override
 	public Object pop()
 	{
-		Node temp = topNode;
-		topNode = topNode.next;
-		size--;
-		return temp.elem;
+		Node temp = topNode;		//saving a reference to the top node before poping
+		topNode = topNode.next;		//moving the top note ref down
+		size--;						//decreasing the size counter
+		return temp.elem;			//returning the element of the popped node; it will be freed at the end of this func block
 	}
 
 	@Override
 	public Object peek()
 	{		
-		return topNode.elem;
+		return topNode.elem;	
 	}
 	
 	public void clear()
 	{
-		topNode = null;
+		topNode = null;		//freeing the linked list by setting the only ref to it to null
 		size = 0;
 	}
 	
+	/**
+	 * A toString for debugging
+	 */	
 	public String toString()
 	{
 		String tempStr = "";
 		Node tempNode = topNode;
 		
-		for (int i=0; i<size; i++)
+		for (int i=0; i<size; i++)			//print out all the nodes in the linked list stack with a loop
 		{
 			tempStr += tempNode.elem + " ";
 			tempNode = tempNode.next;
