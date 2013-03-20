d67cfbfc7035e64fc0f7a3fdb2487355c7d20e0e
Spave <excsniper@gmail.com>
2013-03-20 03:28:00 -0400

    Added comments for Search and the writeup for it

--- Search/src/SearchArray.java
+++ Search/src/SearchArray.java
@@ -1,21 +1,24 @@
 
 public class SearchArray<T>
 {
-	
 	/**
-	 * 
+	 * A tail-recursive search method with O(n) efficiency for internal use.
+	 * row, col are the indices of the cell to be checked.
 	 */
 	private static <T extends Comparable<? super T>> boolean doSearch(T[][] data, T desiredItem, int row, int col)
-	{		
+	{	
+		// base case: if we reach the edge if the array and the item still hasn't been found then it's not in the array
 		if (row == data.length || col < 0)
 			return false;
 		
+		// return true if the item is found in the current cell
 		if (data[row][col].compareTo(desiredItem) == 0)
 			return true;
 
+		// if the current cell is less than the item, we can eliminate the whole row and move one row down
 		if (data[row][col].compareTo(desiredItem) < 0)
 			return doSearch(data, desiredItem, row+1, col);
-		else
+		else // otherwise the cell must be greater than the item and we start looking one col left
 			return doSearch(data, desiredItem, row, col-1);
 	}
 	
@@ -27,10 +30,11 @@ public class SearchArray<T>
 	*/
 	public static <T extends Comparable<? super T>>	boolean search(T[][] data, T desiredItem)
 	{
+		// check if the item is less than the top left or greater than bottom right...
 		if (data[0][0].compareTo(desiredItem) > 0 || data[data.length-1][data[0].length-1].compareTo(desiredItem) < 0)
-			return false;
+			return false; // ...if so then we can conclude that the item isn't in the array
 					
-		return doSearch(data, desiredItem, 0, data[0].length-1);
+		return doSearch(data, desiredItem, 0, data[0].length-1); // run the recursive search method starting from the top right
 	}
 	
 	/** 
@@ -42,11 +46,13 @@ public class SearchArray<T>
 		{
 			for(int c=0; c < data[r].length; c++)
 			{
+				// check every row and col and only return true if item has been found
 				if (data[r][c].compareTo(desiredItem) == 0)
 					return true;
 			}
 		}
 		
+		// otherwise return false
 		return false;
 	}
 }
